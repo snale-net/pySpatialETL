fix spatial resize computation & optimize speed

spatialetl/coverage/Coverage.py
@@ -130,10 +130,10 @@ Soit l'axe y en premier puis l'axe x. Exemple : [y,x]
             ymin = np.min(idx[0])
             ymax = np.max(idx[0]) + 1
 
-            self.source_global_axis_x = self.source_global_axis_x[xmin:xmax]
-            self.source_global_x_size = xmax - xmin
-            self.source_global_axis_y = self.source_global_axis_y[ymin:ymax]
-            self.source_global_y_size = ymax - ymin
+            self.target_global_axis_x = self.source_global_axis_x[xmin:xmax]
+            self.target_global_x_size = xmax - xmin
+            self.target_global_axis_y = self.source_global_axis_y[ymin:ymax]
+            self.target_global_y_size = ymax - ymin
 
         else:
 
@@ -147,20 +147,14 @@ Soit l'axe y en premier puis l'axe x. Exemple : [y,x]
             xmin = np.min(idx[1])
             xmax = np.max(idx[1])+1
 
-            self.source_global_axis_x = self.source_global_axis_x[ymin:ymax, xmin:xmax]
-            self.source_global_x_size = xmax - xmin
-            self.source_global_axis_y = self.source_global_axis_y[ymin:ymax, xmin:xmax]
-            self.source_global_y_size = ymax - ymin
-
-        # source_global sont réduit au zoom
+            self.target_global_axis_x = self.source_global_axis_x[ymin:ymax, xmin:xmax]
+            self.target_global_x_size = xmax - xmin
+            self.target_global_axis_y = self.source_global_axis_y[ymin:ymax, xmin:xmax]
+            self.target_global_y_size = ymax - ymin
 
         # On calcule la grille de destination
         self.target_global_res_x = None
         self.target_global_res_y = None
-        self.target_global_axis_x = self.source_global_axis_x
-        self.target_global_axis_y = self.source_global_axis_y
-        self.target_global_x_size = self.source_global_x_size
-        self.target_global_y_size = self.source_global_y_size
 
         if resolution_x is not None and resolution_y is not None:
 
@@ -292,82 +286,80 @@ Soit l'axe y en premier puis l'axe x. Exemple : [y,x]
 
     def update_mpi_map(self):
 
-        if self.horizontal_resampling:
+        if self.is_regular_grid(type="source"):
 
-            if self.is_regular_grid(type="source"):
+            idx = np.where((self.source_global_axis_x >= np.min(self.read_axis_x(type="target", with_overlap=False))) &
+                           (self.source_global_axis_x <= np.max(self.read_axis_x(type="target", with_overlap=False))))
 
-                idx = np.where((self.source_global_axis_x >= Xmin) &
-                               (self.source_global_axis_x <= Xmax))
+            xmin = np.min(idx[0])
+            xmax = np.max(idx[0]) + 1
 
-                xmin = np.min(idx[0])
-                xmax = np.max(idx[0]) + 1
+            idx = np.where((self.source_global_axis_y >= np.min(self.read_axis_y(type="target", with_overlap=False))) &
+                           (self.source_global_axis_y <= np.max(self.read_axis_y(type="target", with_overlap=False))))
 
-                idx = np.where((self.source_global_axis_y >= Ymin) &
-                               (self.source_global_axis_y <= Ymax))
+            ymin = np.min(idx[0])
+            ymax = np.max(idx[0]) + 1
 
-                ymin = np.min(idx[0])
-                ymax = np.max(idx[0]) + 1
+        else:
 
-            else:
+            idx = np.where(
+                (self.source_global_axis_x >= np.min(self.read_axis_x(type="target", with_overlap=False))) &
+                (self.source_global_axis_x <= np.max(self.read_axis_x(type="target", with_overlap=False))) &
+                (self.source_global_axis_y >= np.min(self.read_axis_y(type="target", with_overlap=False))) &
+                (self.source_global_axis_y <= np.max(self.read_axis_y(type="target", with_overlap=False))))
 
-                idx = np.where(
-                    (self.source_global_axis_x >= np.min(self.read_axis_x(type="target", with_overlap=False))) &
-                    (self.source_global_axis_x <= np.max(self.read_axis_x(type="target", with_overlap=False))) &
-                    (self.source_global_axis_y >= np.min(self.read_axis_y(type="target", with_overlap=False))) &
-                    (self.source_global_axis_y <= np.max(self.read_axis_y(type="target", with_overlap=False))))
+            ymin = np.min(idx[0])
+            ymax = np.max(idx[0]) + 1
+            xmin = np.min(idx[1])
+            xmax = np.max(idx[1]) + 1
 
-                ymin = np.min(idx[0])
-                ymax = np.max(idx[0]) + 1
-                xmin = np.min(idx[1])
-                xmax = np.max(idx[1]) + 1
+        # Version 2
+        # SRC GLOBAL
+        self.map_mpi[self.rank]["src_global_x"] = np.s_[xmin:xmax]
+        self.map_mpi[self.rank]["src_global_x_size"] = xmax - xmin
+        self.map_mpi[self.rank]["src_global_y"] = np.s_[ymin:ymax]
+        self.map_mpi[self.rank]["src_global_y_size"] = ymax - ymin
 
-            # Version 2
-            # SRC GLOBAL
-            self.map_mpi[self.rank]["src_global_x"] = np.s_[xmin:xmax]
-            self.map_mpi[self.rank]["src_global_x_size"] = xmax - xmin
-            self.map_mpi[self.rank]["src_global_y"] = np.s_[ymin:ymax]
-            self.map_mpi[self.rank]["src_global_y_size"] = ymax - ymin
+        dst_global_x_min_overlap = max(0, self.map_mpi[self.rank][
+            "src_global_x"].start - Coverage.HORIZONTAL_OVERLAPING_SIZE)
+        dst_global_x_max_overlap = min(self.source_global_x_size,
+                                       self.map_mpi[self.rank][
+                                           "src_global_x"].stop + Coverage.HORIZONTAL_OVERLAPING_SIZE)
+        self.map_mpi[self.rank]["src_global_x_overlap"] = np.s_[
+                                                          dst_global_x_min_overlap:dst_global_x_max_overlap]
 
-            dst_global_x_min_overlap = max(0, self.map_mpi[self.rank][
-                "src_global_x"].start - Coverage.HORIZONTAL_OVERLAPING_SIZE)
-            dst_global_x_max_overlap = min(self.source_global_x_size,
-                                           self.map_mpi[self.rank][
-                                               "src_global_x"].stop + Coverage.HORIZONTAL_OVERLAPING_SIZE)
-            self.map_mpi[self.rank]["src_global_x_overlap"] = np.s_[
-                                                              dst_global_x_min_overlap:dst_global_x_max_overlap]
+        dst_global_y_min_overlap = max(0, self.map_mpi[self.rank][
+            "src_global_y"].start - Coverage.HORIZONTAL_OVERLAPING_SIZE)
+        dst_global_y_max_overlap = min(self.source_global_y_size,
+                                       self.map_mpi[self.rank][
+                                           "src_global_y"].stop + Coverage.HORIZONTAL_OVERLAPING_SIZE)
+        self.map_mpi[self.rank]["src_global_y_overlap"] = np.s_[
+                                                          dst_global_y_min_overlap:dst_global_y_max_overlap]
 
-            dst_global_y_min_overlap = max(0, self.map_mpi[self.rank][
-                "src_global_y"].start - Coverage.HORIZONTAL_OVERLAPING_SIZE)
-            dst_global_y_max_overlap = min(self.source_global_y_size,
-                                           self.map_mpi[self.rank][
-                                               "src_global_y"].stop + Coverage.HORIZONTAL_OVERLAPING_SIZE)
-            self.map_mpi[self.rank]["src_global_y_overlap"] = np.s_[
-                                                              dst_global_y_min_overlap:dst_global_y_max_overlap]
+        self.map_mpi[self.rank]["src_global_x_size_overlap"] = self.map_mpi[self.rank][
+                                                                   "src_global_x_overlap"].stop - \
+                                                               self.map_mpi[self.rank][
+                                                                   "src_global_x_overlap"].start
+        self.map_mpi[self.rank]["src_global_y_size_overlap"] = self.map_mpi[self.rank][
+                                                                   "src_global_y_overlap"].stop - \
+                                                               self.map_mpi[self.rank][
+                                                                   "src_global_y_overlap"].start
 
-            self.map_mpi[self.rank]["src_global_x_size_overlap"] = self.map_mpi[self.rank][
-                                                                       "src_global_x_overlap"].stop - \
-                                                                   self.map_mpi[self.rank][
-                                                                       "src_global_x_overlap"].start
-            self.map_mpi[self.rank]["src_global_y_size_overlap"] = self.map_mpi[self.rank][
-                                                                       "src_global_y_overlap"].stop - \
-                                                                   self.map_mpi[self.rank][
-                                                                       "src_global_y_overlap"].start
+        self.map_mpi[self.rank]["src_local_x_size"] = xmax - xmin
+        self.map_mpi[self.rank]["src_local_y_size"] = ymax - ymin
+        self.map_mpi[self.rank]["src_local_x"] = np.s_[0:self.map_mpi[self.rank]["src_local_x_size"]]
+        self.map_mpi[self.rank]["src_local_y"] = np.s_[0:self.map_mpi[self.rank]["src_local_y_size"]]
 
-            self.map_mpi[self.rank]["src_local_x_size"] = xmax - xmin
-            self.map_mpi[self.rank]["src_local_y_size"] = ymax - ymin
-            self.map_mpi[self.rank]["src_local_x"] = np.s_[0:self.map_mpi[self.rank]["src_local_x_size"]]
-            self.map_mpi[self.rank]["src_local_y"] = np.s_[0:self.map_mpi[self.rank]["src_local_y_size"]]
+        # OVERLAP
+        self.map_mpi[self.rank]["src_local_x_size_overlap"] = self.map_mpi[self.rank][
+            "src_global_x_size_overlap"]
+        self.map_mpi[self.rank]["src_local_y_size_overlap"] = self.map_mpi[self.rank][
+            "src_global_y_size_overlap"]
 
-            # OVERLAP
-            self.map_mpi[self.rank]["src_local_x_size_overlap"] = self.map_mpi[self.rank][
-                "src_global_x_size_overlap"]
-            self.map_mpi[self.rank]["src_local_y_size_overlap"] = self.map_mpi[self.rank][
-                "src_global_y_size_overlap"]
-
-            self.map_mpi[self.rank]["src_local_x_overlap"] = np.s_[
-                                                             0:self.map_mpi[self.rank]["src_local_x_size_overlap"]]
-            self.map_mpi[self.rank]["src_local_y_overlap"] = np.s_[
-                                                             0:self.map_mpi[self.rank]["src_local_y_size_overlap"]]
+        self.map_mpi[self.rank]["src_local_x_overlap"] = np.s_[
+                                                         0:self.map_mpi[self.rank]["src_local_x_size_overlap"]]
+        self.map_mpi[self.rank]["src_local_y_overlap"] = np.s_[
+                                                         0:self.map_mpi[self.rank]["src_local_y_size_overlap"]]
 
     # Read metadata
     def read_metadata(self):

spatialetl/coverage/LevelCoverage.py
@@ -18,6 +18,7 @@ from spatialetl.coverage.Coverage import Coverage
 from scipy import spatial
 import numpy as np
 import logging
+from itertools import product
 
 class LevelCoverage(Coverage):    
     """
@@ -36,8 +37,6 @@ Elle rajoute une dimension verticale à la couverture horizontale classique.
 
         self.source_global_z_size = self.reader.get_z_size()
         self.source_global_axis_z = self.reader.read_axis_z();
-        self.target_global_z_size = self.source_global_z_size
-        self.target_global_axis_z = self.source_global_axis_z
 
         if self.source_global_axis_z.ndim == 3:
             self.source_sigma_coordinate = True
@@ -46,20 +45,44 @@ Elle rajoute une dimension verticale à la couverture horizontale classique.
         else:
             raise ValueError("[LevelCoverage] Unable to recognize the type of the vertical grid.")
 
+        if zbox == None:
+            # we compute the destination grid
+            Zmin = np.min(self.source_global_axis_z)
+            Zmax = np.max(self.source_global_axis_z)
+        else:
+            Zmin = zbox[0]
+            Zmax = zbox[1]
+
+        if not self.source_sigma_coordinate:
+            idx = np.where((self.source_global_axis_z >= Zmin) &
+                           (self.source_global_axis_z <= Zmax))
+
+            zmin = np.min(idx[0])
+            zmax = np.max(idx[0]) + 1
+
+            self.target_global_axis_z = self.source_global_axis_z[zmin:zmax]
+            self.target_global_z_size = zmax - zmin
+        else:
+            idx = np.where((self.source_global_axis_z[:,0:self.get_y_size(type="source_global"),0:self.get_x_size(type="source_global")] >= Zmin) &
+                           (self.source_global_axis_z[:,0:self.get_y_size(type="source_global"),0:self.get_x_size(type="source_global")] <= Zmax))
+
+
+            zmin = np.min(idx[0])
+            zmax = np.max(idx[0]) + 1
+
+            self.target_global_axis_z = self.source_global_axis_z[zmin:zmax,0:self.get_y_size(type="source_global"),0:self.get_x_size(type="source_global")]
+            self.target_global_z_size = zmax - zmin
+
+        # source_global sont réduit au zoom
+
+        # On calcule la grille de destination
+        self.target_sigma_coordinate = self.source_sigma_coordinate
+
         if resolution_z is not None:
 
             self.vertical_resampling = True
             self.target_sigma_coordinate = False
-
-            if zbox == None:
-                # we compute the destination grid
-                zmin = np.min(self.source_global_axis_z)
-                zmax = np.max(self.source_global_axis_z)
-            else:
-                zmin = zbox[0]
-                zmax = zbox[1]
-
-            self.target_global_axis_z = np.arange(zmin, zmax, resolution_z)
+            self.target_global_axis_z = np.arange(Zmin, Zmax, resolution_z)
             self.target_global_z_size = len(self.target_global_axis_z)
 
             if self.rank == 0:
@@ -73,11 +96,6 @@ Elle rajoute une dimension verticale à la couverture horizontale classique.
 
                 logging.info(
                     '[vertical_interpolation] Target grid size : ' + str(self.target_global_z_size) + " level(s)")
-
-        else:
-            self.target_global_axis_z = self.source_global_axis_z
-            self.target_global_z_size = self.source_global_z_size
-            self.target_sigma_coordinate = self.source_sigma_coordinate
 
         self.depth_weight = {}
 
@@ -183,7 +201,7 @@ Elle rajoute une dimension verticale à la couverture horizontale classique.
                     x =  idx[2][index]
                     y =  idx[1][index]
 
-                    if vert_coord[y,x] is None:
+                    if vert_coord[y,x] is None: #first time
                         vert_coord[y, x] = []
 
                     vert_coord[y, x].append((int(index_z)))
@@ -246,46 +264,67 @@ Elle rajoute une dimension verticale à la couverture horizontale classique.
 
         else: # Cas de grille classique
 
-            logging.debug("[LevelCoverage][find_level_index()] Looking for : " + str(depth) + " m water depth")
+            if self.rank == 0:
+                logging.debug("[LevelCoverage][find_level_index()] Looking for : " + str(
+                    depth) + " m water depth with an interval of +/- " + str(LevelCoverage.DEPTH_DELTA) + " m")
 
-            # On cherche l'index le plus proche
-            array = np.asarray(self.source_global_axis_z)
-            index_z = (np.abs(array - depth)).argmin()
+            if method == "fast":
 
-            if abs(depth - self.source_global_axis_z[index_z]) <= LevelCoverage.DEPTH_DELTA:
-            # On a trouvé une profondeur qui correspond au delta près.
+                X = np.abs(self.source_global_axis_z - depth)
+                idx = np.where(X <= LevelCoverage.DEPTH_DELTA)
 
-                logging.debug(
-                "[LevelCoverage][find_level_index()] found : " + str(
-                    self.source_global_axis_z[index_z]) + " m water depth")
+                for index in range(np.shape(idx)[1]):
+                    index_z = idx[0][index]
 
-                for y in range(0, ymax):
-                    for x in range(0, xmax):
+                    for y,x in product(range(0,ymax),range(0,xmax)):
+                        if vert_coord[y, x] is None:  # first time
+                            vert_coord[y, x] = []
 
-                        vert_coord[y, x] = []
-                        vert_coord[y,x].append((int(index_z)))
-                        indexes_z.append((int(index_z)))
+                        vert_coord[y, x].append((int(index_z)))
 
-                        if abs(depth - self.source_global_axis_z[index_z]) != 0.0:
-                            # On n'a pas trouvé exactement notre profondeur, on va chercher autour au DEPTH_DELTA près
-                            zz = index_z
+                        if int(index_z) not in indexes_z:
+                            indexes_z.append((int(index_z)))
 
-                            while zz - 1 >= 0 and abs(self.source_global_axis_z[zz - 1] - depth) <= LevelCoverage.DEPTH_DELTA and zz -1 not in vert_coord[y,x]:
-                                logging.debug(
-                                    "[LevelCoverage][find_level_index()] found : " + str(
-                                        self.source_global_axis_z[zz-1]) + " m water depth")
+            elif method == "classic":
 
-                                vert_coord[y,x].append((int(zz - 1)))
-                                indexes_z.append((int(zz - 1)))
-                                zz = zz - 1
+                # On cherche l'index le plus proche
+                array = np.asarray(self.source_global_axis_z)
+                index_z = (np.abs(array - depth)).argmin()
 
-                            while zz + 1 < self.get_z_size() and abs(self.source_global_axis_z[zz + 1] - depth) <= LevelCoverage.DEPTH_DELTA and zz +1 not in vert_coord[y,x]:
-                                logging.debug(
-                                    "[LevelCoverage][find_level_index()] found : " + str(
-                                        self.source_global_axis_z[zz+1]) + " m water depth")
-                                vert_coord[y,x].append((int(zz + 1)))
-                                indexes_z.append((int(zz + 1)))
-                                zz = zz + 1
+                if abs(depth - self.source_global_axis_z[index_z]) <= LevelCoverage.DEPTH_DELTA:
+                # On a trouvé une profondeur qui correspond au delta près.
+
+                    logging.debug(
+                    "[LevelCoverage][find_level_index()] found : " + str(
+                        self.source_global_axis_z[index_z]) + " m water depth")
+
+                    for y in range(0, ymax):
+                        for x in range(0, xmax):
+
+                            vert_coord[y, x] = []
+                            vert_coord[y,x].append((int(index_z)))
+                            indexes_z.append((int(index_z)))
+
+                            if abs(depth - self.source_global_axis_z[index_z]) != 0.0:
+                                # On n'a pas trouvé exactement notre profondeur, on va chercher autour au DEPTH_DELTA près
+                                zz = index_z
+
+                                while zz - 1 >= 0 and abs(self.source_global_axis_z[zz - 1] - depth) <= LevelCoverage.DEPTH_DELTA and zz -1 not in vert_coord[y,x]:
+                                    logging.debug(
+                                        "[LevelCoverage][find_level_index()] found : " + str(
+                                            self.source_global_axis_z[zz-1]) + " m water depth")
+
+                                    vert_coord[y,x].append((int(zz - 1)))
+                                    indexes_z.append((int(zz - 1)))
+                                    zz = zz - 1
+
+                                while zz + 1 < self.get_z_size() and abs(self.source_global_axis_z[zz + 1] - depth) <= LevelCoverage.DEPTH_DELTA and zz +1 not in vert_coord[y,x]:
+                                    logging.debug(
+                                        "[LevelCoverage][find_level_index()] found : " + str(
+                                            self.source_global_axis_z[zz+1]) + " m water depth")
+                                    vert_coord[y,x].append((int(zz + 1)))
+                                    indexes_z.append((int(zz + 1)))
+                                    zz = zz + 1
 
             else:
                 logging.debug("[LevelCoverage] " + str(depth) + " m water depth was not found on the Z axis.")

spatialetl/coverage/TimeLevelCoverage.py
@@ -19,6 +19,7 @@ from spatialetl.coverage.LevelCoverage import LevelCoverage
 from spatialetl.coverage.TimeCoverage import TimeCoverage
 from spatialetl.operator.interpolator.InterpolatorCore import resample_2d_to_grid
 from spatialetl.operator.interpolator.InterpolatorCore import vertical_interpolation
+from itertools import product
 import numpy as np
 import logging
 
@@ -93,8 +94,7 @@ Elle rajoute les dimensions temporelle et verticale à la couverture horizontale
                 if len(vert_coord[y, x]) == 1:
                     # Il n'y a qu'une seule couche de sélectionner donc pas d'interpolation possible
                     # On retrouve l'index de la layer
-                    array = np.asarray(indexes_z)
-                    index_layer = (np.abs(array - vert_coord[y, x][0])).argmin()
+                    index_layer = (np.abs(indexes_z - vert_coord[y, x][0])).argmin()
                     data[y, x] = layers[index_layer, y, x]
 
                 elif len(vert_coord[y, x]) > 1:
@@ -104,8 +104,7 @@ Elle rajoute les dimensions temporelle et verticale à la couverture horizontale
 
                     for z in range(0, len(vert_coord[y, x])):
                         # On retrouve l'index de la layer
-                        array = np.asarray(indexes_z)
-                        index_layer = (np.abs(array - vert_coord[y, x][z])).argmin()
+                        index_layer = (np.abs(indexes_z - vert_coord[y, x][z])).argmin()
 
                         if self.is_sigma_coordinate(type="source"):
                             candidateDepths[z] = z_axis[index_layer, y, x]
@@ -152,6 +151,7 @@ Elle rajoute les dimensions temporelle et verticale à la couverture horizontale
         data[:] = np.NAN
         targetDepth = [depth]
 
+
         mask_t = self.reader.read_variable_2D_sea_binary_mask(
             self.map_mpi[self.rank]["src_global_x_overlap"].start,
             self.map_mpi[self.rank]["src_global_x_overlap"].stop,
@@ -176,8 +176,7 @@ Elle rajoute les dimensions temporelle et verticale à la couverture horizontale
                 if len(vert_coord[y, x]) == 1:
                     # Il n'y a qu'une seule couche de sélectionner donc pas d'interpolation possible
                     # On retrouve l'index de la layer
-                    array = np.asarray(indexes_z)
-                    index_layer = (np.abs(array - vert_coord[y, x][0])).argmin()
+                    index_layer = (np.abs(indexes_z - vert_coord[y, x][0])).argmin()
                     data[y, x] = layers[index_layer, y, x]
 
                 elif len(vert_coord[y, x]) > 1:
@@ -187,8 +186,7 @@ Elle rajoute les dimensions temporelle et verticale à la couverture horizontale
 
                     for z in range(0, len(vert_coord[y, x])):
                         # On retrouve l'index de la layer
-                        array = np.asarray(indexes_z)
-                        index_layer = (np.abs(array - vert_coord[y, x][z])).argmin()
+                        index_layer = (np.abs(indexes_z - vert_coord[y, x][z])).argmin()
 
                         if self.is_sigma_coordinate(type="source"):
                             candidateDepths[z] = z_axis[index_layer, y, x]
@@ -258,8 +256,7 @@ Elle rajoute les dimensions temporelle et verticale à la couverture horizontale
 
                         # Il n'y a qu'une seule couche de sélectionner donc pas d'interpolation possible
                         # On retrouve l'index de la layer
-                        array = np.asarray(indexes_z)
-                        index_layer = (np.abs(array - vert_coord[y, x][0])).argmin()
+                        index_layer = (np.abs(indexes_z - vert_coord[y, x][0])).argmin()
 
                         data[0][y, x] = layers[index_layer,0, y, x]
                         data[1][y, x] = layers[index_layer,1, y, x]
@@ -271,8 +268,7 @@ Elle rajoute les dimensions temporelle et verticale à la couverture horizontale
 
                         for z in range(0, len(vert_coord[y, x])):
                             # On retrouve l'index de la layer
-                            array = np.asarray(indexes_z)
-                            index_layer = (np.abs(array - vert_coord[y, x][z])).argmin()
+                            index_layer = (np.abs(indexes_z - vert_coord[y, x][z])).argmin()
 
                             candidateDepths[0][z] = self.source_global_axis_z[index_layer,0, y, x]
                             candidateValues[1][z] = layers[index_layer,1, y, x]

spatialetl/coverage/io/netcdf/mercator/MercatorReader.py
@@ -15,131 +15,137 @@
 #
 from __future__ import division, print_function, absolute_import
 from spatialetl.coverage.io.CoverageReader import CoverageReader
+from spatialetl.utils.VariableDefinition import VariableDefinition
 from spatialetl.coverage.TimeCoverage import TimeCoverage
-from netCDF4 import Dataset, num2date
+from netCDF4 import Dataset, MFDataset, num2date
+from spatialetl.exception.VariableNameError import VariableNameError
 import numpy as np
+import os
+import logging
 
 class MercatorReader(CoverageReader):
     
-    def __init__(self,m,d,t,u,v):
-        CoverageReader.__init__(self,m)
-        self.mask = Dataset(m, 'r')
-        self.grid2D = Dataset(d, 'r')
-        self.gridT = Dataset(t, 'r') 
-        self.gridU = Dataset(u, 'r')  
-        self.gridV = Dataset(v, 'r') 
-     
-    # Axis
-    def read_axis_t(self,timestamp):
-        """Attention si gridT, U,V,2D ont un time_counter different"""
-        data = self.gridT.variables['time_counter'][:]
-        result = num2date(data, units = self.gridT.variables['time_counter'].units, calendar = self.gridT.variables['time_counter'].calendar)
-        
-        if timestamp ==1:           
-            return [ (t - TimeCoverage.TIME_DATUM).total_seconds() \
-                for t in result];
-        else:            
+    def __init__(self,myFile):
+        CoverageReader.__init__(self,myFile)
+
+        if os.path.isfile(self.filename):
+            self.ncfile = Dataset(self.filename, 'r')
+        elif os.path.isdir(self.filename):
+            self.ncfile = MFDataset(os.path.join(self.filename,"*.nc"), 'r')
+        elif self.filename.endswith("*"):
+            self.ncfile = MFDataset(self.filename+".nc", 'r')
+        else:
+            raise ValueError("Unable to decode file "+str(self.filename))
+
+    def is_regular_grid(self):
+        return True
+
+    def get_x_size(self):
+        return np.shape(self.ncfile.variables['longitude'][:])[0];
+
+    def get_y_size(self):
+        return np.shape(self.ncfile.variables['latitude'][:])[0];
+
+    def get_z_size(self):
+        return np.shape(self.ncfile.variables['depth'][:])[0];
+
+    def get_t_size(self):
+        return np.shape(self.ncfile.variables['time'])[0];
+
+    def read_axis_x(self, xmin, xmax, ymin, ymax):
+        return self.ncfile.variables['longitude'][xmin:xmax]
+
+    def read_axis_y(self, xmin, xmax, ymin, ymax):
+        return self.ncfile.variables['latitude'][ymin:ymax]
+
+    def read_axis_z(self, ):
+        lev = self.ncfile.variables["depth"][:]
+        # lev = np.ma.filled(self.grid.variables["depth_t"], fill_value=np.nan)
+        # lev = np.ma.filled(mx, fill_value=np.nan)
+        return lev
+
+    def read_axis_t(self, tmin, tmax, timestamp):
+        data = self.ncfile.variables['time'][tmin:tmax]
+        result = num2date(data, units=self.ncfile.variables['time'].units, calendar=self.ncfile.variables['time'].calendar)
+
+        if timestamp == 1:
+            return [(t - TimeCoverage.TIME_DATUM).total_seconds() \
+                    for t in result];
+        else:
             return result
-    
-    def read_axis_x(self):        
-        return self.gridT.variables['nav_lon'][:]
-    
-    def read_axis_y(self):        
-        return self.gridT.variables['nav_lat'][:]
-    
-    def read_axis_z(self):       
-        return self.gridT.variables['deptht']
-    
-    # Data    
-    def read_variable_2D_sea_binary_mask(self):
-        return self.mask.variables["tmask"][0][0][:]
 
-    def read_variable_3D_sea_binary_mask(self):
-        return self.mask.variables["tmask"][0][:]
+    def read_variable_2D_sea_binary_mask(self, xmin, xmax, ymin, ymax):
+        try:
+            if "zos" in self.ncfile.variables:
+                mask = np.ma.filled(self.ncfile.variables["zos"][0,ymin:ymax, xmin:xmax], fill_value=np.nan)
+                mask[mask != np.nan]=1
+                return mask
+        except Exception as ex:
+            logging.debug("Error '" + str(ex) + "'")
+            raise (VariableNameError("MercatorReader", "An error occured : '" + str(ex) + "'", 1000))
 
-    def read_variable_4D_sea_binary_mask(self):
-        return self.mask.variables["tmask"]
-    
-    def read_variable_sea_surface_height_above_sea_level_at_time(self,t):
-        return self.grid2D.variables["sossheig"][t][:]
-     
-    def read_variable_baroclinic_sea_water_velocity_at_time_and_depth(self,index_t,index_z,depth):
+        logging.debug("No variables found for '" + str(VariableDefinition.LONG_NAME['2d_sea_binary_mask']) + "'")
+        raise (VariableNameError("MercatorReader",
+                                 "No variables found for '" + str(
+                                     VariableDefinition.LONG_NAME['2d_sea_binary_mask']) + "'",
+                                 1000))
 
-        mask_t = self.read_variable_4D_sea_binary_mask();
-        mask_u = self.mask.variables["umask"][:];
-        mask_v = self.mask.variables["vmask"][:];
-        lon_t = self.read_axis_x();
-        lat_t = self.read_axis_y();
-        data_u = self.gridU.variables["vozocrtx"][index_t][::]
-        data_v = self.gridV.variables["vomecrty"][index_t][::]
-        
-        # compute and apply rotation matrix
-        xmax=np.shape(lon_t)[1]
-        ymax=np.shape(lon_t)[0]
-        gridrotcos_t = np.zeros([ymax,xmax])
-        gridrotsin_t = np.zeros([ymax,xmax])       
-        
-        u = np.zeros([ymax,xmax])
-        u[:] = np.NAN
-        v = np.zeros([ymax,xmax])
-        v[:] = np.NAN
-        u_rot = np.zeros([ymax,xmax])
-        u_rot[:] = np.NAN
-        v_rot = np.zeros([ymax,xmax])
-        v_rot[:] = np.NAN
+    #################
+    # HYDRO
+    # Sea Surface
+    #################
 
-        # We process point inside the domain
-        for y in range(1,ymax-1):
-            for x in range(1,xmax-1):
-                
-                x1=(lon_t[y,x+1]-lon_t[y,x-1])*np.pi/180.
-                if(x1<-np.pi): x1=x1+2.*np.pi
-                if(x1> np.pi): x1=x1-2.*np.pi
-                x0=-np.arctan2((lat_t[y,x+1]-lat_t[y,x-1])*np.pi/180.,x1*np.cos(lat_t[y,x]*np.pi/180.))
-                gridrotcos_t[y,x]=np.cos(x0)
-                gridrotsin_t[y,x]=np.sin(x0)
+    def read_variable_sea_surface_height_above_geoid_at_time(self, index_t, xmin, xmax, ymin, ymax):
+        try:
+            if "zos" in self.ncfile.variables:
+                return np.ma.filled(self.ncfile.variables["zos"][index_t,ymin:ymax, xmin:xmax], fill_value=np.nan)
+        except Exception as ex:
+            logging.debug("Error '" + str(ex) + "'")
+            raise (VariableNameError("MercatorReader", "An error occured : '" + str(ex) + "'", 1000))
 
-                if (mask_t[0,index_z[y,x],y,x] == 1.):
-                    
-                    u_left = 0
-                    u_right = 0
-                    v_down = 0
-                    v_up = 0
-                   
-                    if (mask_u[0,index_z[y,x-1],y,x-1] == 1.):
-                        u_left = data_u[index_z[y,x-1],y,x-1];
+        logging.debug("No variables found for '" + str(
+            VariableDefinition.LONG_NAME['sea_surface_height_above_geoid']) + "'")
+        raise (VariableNameError("MercatorReader",
+                                 "No variables found for '" + str(
+                                     VariableDefinition.LONG_NAME['sea_surface_height_above_geoid']) + "'",
+                                 1000))
 
-                    if (mask_u[0,index_z[y,x],y,x] == 1.):
-                        u_right = data_u[index_z[y,x],y,x];
+        #################
+        # HYDRO
+        # 3D
+        #################
 
-                    if (mask_v[0,index_z[y-1,x],y-1,x] == 1.):
-                        v_down = data_v[index_z[y-1,x],y-1,x];
+    def read_variable_sea_water_temperature_at_time_and_depth(self, index_t, index_z, xmin, xmax, ymin, ymax):
+        try:
+            if "thetao" in self.ncfile.variables:
+                return np.ma.filled(self.ncfile.variables["thetao"][index_t,index_z,ymin:ymax, xmin:xmax],
+                                    fill_value=np.nan)
+        except Exception as ex:
+            logging.debug("Error '" + str(ex) + "'")
+            raise (VariableNameError("SymphonieReader", "An error occured : '" + str(ex) + "'", 1000))
 
-                    if (mask_v[0,index_z[y,x],y,x] == 1.):
-                        v_up = data_v[index_z[y,x],y,x];
+        logging.debug("No variables found for '" + str(
+            VariableDefinition.LONG_NAME['sea_surface_temperature']) + "'")
+        raise (VariableNameError("SymphonieReader",
+                                 "No variables found for '" + str(
+                                     VariableDefinition.LONG_NAME['sea_surface_temperature']) + "'",
+                                 1000))
 
-                    # compute an half-value
-                    u[y,x]=0.5*(u_left+u_right)
-                    v[y,x]=0.5*(v_down+v_up)
-                    
-                    # apply rotation                
-                    u_rot[y,x]=u[y,x]*gridrotcos_t[y,x]+v[y,x]*gridrotsin_t[y,x]
-                    v_rot[y,x]=-u[y,x]*gridrotsin_t[y,x]+v[y,x]*gridrotcos_t[y,x]   
-          
-        # We process boundaries point
-        # bottom        
-        u_rot[0,0:xmax]=u_rot[1,0:xmax]   
-        v_rot[0,0:xmax]=v_rot[1,0:xmax] 
-        # up
-        u_rot[ymax-1,0:xmax]=u_rot[ymax-2,0:xmax] 
-        v_rot[ymax-1,0:xmax]=v_rot[ymax-2,0:xmax] 
-        
-        # left
-        u_rot[0:ymax,0]=u_rot[0:ymax,1]   
-        v_rot[0:ymax,0]=v_rot[0:ymax,1]   
-        # right
-        u_rot[0:ymax,xmax-1]=u_rot[0:ymax,xmax-2]  
-        v_rot[0:ymax,xmax-1]=v_rot[0:ymax,xmax-2]  
+    def read_variable_sea_water_salinity_at_time_and_depth(self, index_t, index_z, xmin, xmax, ymin, ymax):
+        try:
+            if "so" in self.ncfile.variables:
+                return np.ma.filled(self.ncfile.variables["so"][index_t,index_z,ymin:ymax, xmin:xmax],
+                                    fill_value=np.nan)
+        except Exception as ex:
+            logging.debug("Error '" + str(ex) + "'")
+            raise (VariableNameError("SymphonieReader", "An error occured : '" + str(ex) + "'", 1000))
 
-        return [u_rot,v_rot]
+        logging.debug("No variables found for '" + str(
+            VariableDefinition.LONG_NAME['sea_surface_salinity']) + "'")
+        raise (VariableNameError("SymphonieReader",
+                                 "No variables found for '" + str(
+                                     VariableDefinition.LONG_NAME['sea_surface_salinity']) + "'",
+                                 1000))
 
+    def read_variable_baroclinic_sea_water_velocity_at_time_and_depth(self, index_t, index_z, xmin, xmax, ymin, ymax):
+        return [self.ncfile.variables["uo"][index_t,index_z,ymin:ymax, xmin:xmax], self.ncfile.variables["vo"][index_t,index_z,ymin:ymax, xmin:xmax]]

spatialetl/coverage/io/netcdf/symphonie/SymphonieReader.py
@@ -39,7 +39,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
             self.ncfile = Dataset(self.filename, 'r')
         elif os.path.isdir(self.filename):
             self.ncfile = MFDataset(os.path.join(self.filename,"*.nc"), 'r')
-        elif "*" in self.filename:
+        elif self.filename.endswith("*"):
             self.ncfile = MFDataset(self.filename+".nc", 'r')
         else:
             raise ValueError("Unable to decode file "+str(self.filename))
@@ -120,7 +120,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
         index_z = self.get_z_size() - 1 # At surface level
         try:
             if "mask_t" in self.grid.variables:
-                return np.ma.filled(self.grid.variables["mask_t"][index_z][ymin:ymax, xmin:xmax], fill_value=np.nan)
+                return np.ma.filled(self.grid.variables["mask_t"][index_z,ymin:ymax, xmin:xmax], fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
             raise (VariableNameError("SymphonieReader", "An error occured : '" + str(ex) + "'", 1000))
@@ -147,7 +147,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_wet_binary_mask_at_time(self, index_t):
         try:
             if "wetmask_t" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["wetmask_t"][index_t][ymin:ymax, xmin:xmax], fill_value=np.nan)
+                return np.ma.filled(self.ncfile.variables["wetmask_t"][index_t,ymin:ymax, xmin:xmax], fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
             raise (VariableNameError("SymphonieReader", "An error occured : '" + str(ex) + "'", 1000))
@@ -179,7 +179,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_sea_surface_height_above_mean_sea_level_at_time(self, index_t,xmin,xmax,ymin,ymax):
         try:
             if "ssh_w" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["ssh_w"][index_t][ymin:ymax, xmin:xmax], fill_value=np.nan)
+                return np.ma.filled(self.ncfile.variables["ssh_w"][index_t,ymin:ymax, xmin:xmax], fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
             raise (VariableNameError("SymphonieReader", "An error occured : '" + str(ex) + "'", 1000))
@@ -195,7 +195,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
             index_z = self.get_z_size() - 1
 
             if "tem" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["tem"][index_t][index_z][ymin:ymax, xmin:xmax], fill_value=np.nan)
+                return np.ma.filled(self.ncfile.variables["tem"][index_t,index_z,ymin:ymax, xmin:xmax], fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
             raise (VariableNameError("SymphonieReader", "An error occured : '" + str(ex) + "'", 1000))
@@ -212,7 +212,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
             index_z = self.get_z_size() - 1
 
             if "sal" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["sal"][index_t][index_z][ymin:ymax, xmin:xmax],
+                return np.ma.filled(self.ncfile.variables["sal"][index_t,index_z,ymin:ymax, xmin:xmax],
                                     fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
@@ -228,10 +228,10 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_sea_water_velocity_at_sea_water_surface_at_time(self, index_t,xmin,xmax,ymin,ymax):
         index_z = self.zmax - 1
         mask_t = self.read_variable_3D_sea_binary_mask();
-        mask_u = self.grid.variables["mask_u"][index_z][:];
-        mask_v = self.grid.variables["mask_v"][index_z][:];
-        data_u = self.ncfile.variables["vel_u"][index_t][index_z][:]
-        data_v = self.ncfile.variables["vel_v"][index_t][index_z][:]
+        mask_u = self.grid.variables["mask_u"][index_z,:];
+        mask_v = self.grid.variables["mask_v"][index_z,:];
+        data_u = self.ncfile.variables["vel_u"][index_t,index_z,:]
+        data_v = self.ncfile.variables["vel_v"][index_t,index_z,:]
 
         u = np.zeros([self.ymax, self.xmax])
         u[:] = np.NAN
@@ -313,7 +313,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
             index_z = 0
 
             if "tem" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["tem"][index_t][index_z][ymin:ymax, xmin:xmax],
+                return np.ma.filled(self.ncfile.variables["tem"][index_t,index_z,ymin:ymax, xmin:xmax],
                                     fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
@@ -331,7 +331,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
             index_z = 0
 
             if "sal" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["sal"][index_t][index_z][ymin:ymax, xmin:xmax],
+                return np.ma.filled(self.ncfile.variables["sal"][index_t,index_z,ymin:ymax, xmin:xmax],
                                     fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
@@ -347,10 +347,10 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_sea_water_velocity_at_ground_level_at_time(self, index_t,xmin,xmax,ymin,ymax):
         index_z = 0
         mask_t = self.read_variable_3D_sea_binary_mask();
-        mask_u = self.grid.variables["mask_u"][index_z][:];
-        mask_v = self.grid.variables["mask_v"][index_z][:];
-        data_u = self.ncfile.variables["vel_u"][index_t][index_z][:]
-        data_v = self.ncfile.variables["vel_v"][index_t][index_z][:]
+        mask_u = self.grid.variables["mask_u"][index_z,:];
+        mask_v = self.grid.variables["mask_v"][index_z,:];
+        data_u = self.ncfile.variables["vel_u"][index_t,index_z,:]
+        data_v = self.ncfile.variables["vel_v"][index_t,index_z,:]
 
         u = np.zeros([self.ymax, self.xmax])
         u[:] = np.NAN
@@ -567,7 +567,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_sea_water_temperature_at_time_and_depth(self, index_t, index_z,xmin,xmax,ymin,ymax):
         try:
             if "tem" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["tem"][index_t][index_z][ymin:ymax, xmin:xmax],
+                return np.ma.filled(self.ncfile.variables["tem"][index_t,index_z,ymin:ymax, xmin:xmax],
                                     fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
@@ -583,7 +583,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_sea_water_salinity_at_time_and_depth(self, index_t,index_z,xmin,xmax,ymin,ymax):
         try:
             if "sal" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["sal"][index_t][index_z][ymin:ymax, xmin:xmax],
+                return np.ma.filled(self.ncfile.variables["sal"][index_t,index_z,ymin:ymax, xmin:xmax],
                                     fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
@@ -598,10 +598,10 @@ La classe SymphonieReader permet de lire les données du format Symphonie
 
     def read_variable_baroclinic_sea_water_velocity_at_time_and_depth(self, index_t, index_z,xmin,xmax,ymin,ymax):
         mask_t = self.read_variable_3D_sea_binary_mask(xmin,xmax,ymin,ymax);
-        mask_u = self.grid.variables["mask_u"][index_z][:];
-        mask_v = self.grid.variables["mask_v"][index_z][:];
-        data_u = self.ncfile.variables["vel_u"][index_t][index_z][:]
-        data_v = self.ncfile.variables["vel_v"][index_t][index_z][:]
+        mask_u = self.grid.variables["mask_u"][index_z,:];
+        mask_v = self.grid.variables["mask_v"][index_z,:];
+        data_u = self.ncfile.variables["vel_u"][index_t,index_z,:]
+        data_v = self.ncfile.variables["vel_v"][index_t,index_z,:]
 
         u = np.zeros([self.ymax, self.xmax])
         u[:] = np.NAN
@@ -679,7 +679,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_sea_surface_wave_significant_height_at_time(self, index_t,xmin,xmax,ymin,ymax):
         try:
             if "hs_wave_t" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["hs_wave_t"][index_t][ymin:ymax, xmin:xmax], fill_value=np.nan)
+                return np.ma.filled(self.ncfile.variables["hs_wave_t"][index_t,ymin:ymax, xmin:xmax], fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
             raise (VariableNameError("SymphonieReader", "An error occured : '" + str(ex) + "'", 1000))
@@ -693,7 +693,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_sea_surface_wave_mean_period_at_time(self, index_t,xmin,xmax,ymin,ymax):
         try:
             if "t_wave_t" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["t_wave_t"][index_t][ymin:ymax, xmin:xmax], fill_value=np.nan)
+                return np.ma.filled(self.ncfile.variables["t_wave_t"][index_t,ymin:ymax, xmin:xmax], fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
             raise (VariableNameError("SymphonieReader", "An error occured : '" + str(ex) + "'", 1000))
@@ -707,7 +707,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_sea_surface_wave_to_direction_at_time(self, index_t,xmin,xmax,ymin,ymax):
         try:
             if "dir_wave_t" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["dir_wate_t"][index_t][ymin:ymax, xmin:xmax], fill_value=np.nan)
+                return np.ma.filled(self.ncfile.variables["dir_wate_t"][index_t,ymin:ymax, xmin:xmax], fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")
             raise (VariableNameError("SymphonieReader", "An error occured : '" + str(ex) + "'", 1000))
@@ -721,10 +721,10 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_sea_surface_wave_stokes_drift_velocity_at_time(self, index_t,xmin,xmax,ymin,ymax):
         index_z = self.zmax - 1
         mask_t = self.read_variable_2D_sea_binary_mask();
-        mask_u = self.grid.variables["mask_u"][index_z][:];
-        mask_v = self.grid.variables["mask_v"][index_z][:];
-        data_u = self.ncfile.variables["velbarstokes_u"][index_t][::]
-        data_v = self.ncfile.variables["velbarstokes_v"][index_t][::]
+        mask_u = self.grid.variables["mask_u"][index_z,:];
+        mask_v = self.grid.variables["mask_v"][index_z,:];
+        data_u = self.ncfile.variables["velbarstokes_u"][index_t,::]
+        data_v = self.ncfile.variables["velbarstokes_v"][index_t,::]
 
         u = np.zeros([self.ymax, self.xmax])
         u[:] = np.NAN
@@ -803,10 +803,10 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_atmosphere_momentum_flux_to_waves_at_time(self, index_t,xmin,xmax,ymin,ymax):
         index_z = self.zmax - 1
         mask_t = self.read_variable_2D_sea_binary_mask();
-        mask_u = self.grid.variables["mask_u"][index_z][:];
-        mask_v = self.grid.variables["mask_v"][index_z][:];
-        data_u = self.ncfile.variables["tawx"][index_t][::]
-        data_v = self.ncfile.variables["tawy"][index_t][::]
+        mask_u = self.grid.variables["mask_u"][index_z,:];
+        mask_v = self.grid.variables["mask_v"][index_z,:];
+        data_u = self.ncfile.variables["tawx"][index_t,::]
+        data_v = self.ncfile.variables["tawy"][index_t,::]
 
         u = np.zeros([self.ymax, self.xmax])
         u[:] = np.NAN
@@ -880,10 +880,10 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_waves_momentum_flux_to_ocean_at_time(self, index_t,xmin,xmax,ymin,ymax):
         index_z = self.zmax - 1
         mask_t = self.read_variable_2D_sea_binary_mask();
-        mask_u = self.grid.variables["mask_u"][index_z][:];
-        mask_v = self.grid.variables["mask_v"][index_z][:];
-        data_u = self.ncfile.variables["twox"][index_t][::]
-        data_v = self.ncfile.variables["twoy"][index_t][::]
+        mask_u = self.grid.variables["mask_u"][index_z,:];
+        mask_v = self.grid.variables["mask_v"][index_z,:];
+        data_u = self.ncfile.variables["twox"][index_t,::]
+        data_v = self.ncfile.variables["twoy"][index_t,::]
 
         u = np.zeros([self.ymax, self.xmax])
         u[:] = np.NAN
@@ -967,10 +967,10 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_wind_stress_at_time(self, index_t,xmin,xmax,ymin,ymax):
         index_z = self.zmax - 1
         mask_t = self.read_variable_2D_sea_binary_mask();
-        mask_u = self.grid.variables["mask_u"][index_z][:];
-        mask_v = self.grid.variables["mask_v"][index_z][:];
-        data_u = self.ncfile.variables["wstress_u"][index_t][::]
-        data_v = self.ncfile.variables["wstress_v"][index_t][::]
+        mask_u = self.grid.variables["mask_u"][index_z,:];
+        mask_v = self.grid.variables["mask_v"][index_z,:];
+        data_u = self.ncfile.variables["wstress_u"][index_t,::]
+        data_v = self.ncfile.variables["wstress_v"][index_t,::]
 
         u = np.zeros([self.ymax, self.xmax])
         u[:] = np.NAN
@@ -1049,8 +1049,8 @@ La classe SymphonieReader permet de lire les données du format Symphonie
         mask_t = self.read_variable_2D_sea_binary_mask();
         lon_t = self.read_axis_x();
         lat_t = self.read_axis_y();
-        data_u = self.ncfile.variables["uwind_t"][index_t][::]
-        data_v = self.ncfile.variables["vwind_t"][index_t][::]
+        data_u = self.ncfile.variables["uwind_t"][index_t,::]
+        data_v = self.ncfile.variables["vwind_t"][index_t,::]
 
         u_rot = np.zeros([self.ymax, self.xmax])
         u_rot[:] = np.NAN
@@ -1102,7 +1102,7 @@ La classe SymphonieReader permet de lire les données du format Symphonie
     def read_variable_bathy_ssh_at_time(self,index_t,xmin,xmax,ymin,ymax):
         try:
             if "hssh" in self.ncfile.variables:
-                return np.ma.filled(self.ncfile.variables["hssh"][index_t][ymin:ymax, xmin:xmax],
+                return np.ma.filled(self.ncfile.variables["hssh"][index_t,ymin:ymax, xmin:xmax],
                                     fill_value=np.nan)
         except Exception as ex:
             logging.debug("Error '" + str(ex) + "'")

spatialetl/operator/interpolator/InterpolatorCore.py
@@ -47,18 +47,14 @@ def vertical_interpolation(sourceAxis,targetAxis,data,method):
     logging.debug("[InterpolatorCore][vertical_interpolation()] Method: " + str(method))
     logging.debug("[InterpolatorCore][vertical_interpolation()] ----------------------------------------")
 
-    if method is None:
-        return np.nan
-
-    elif method == "mean":
+    if method == "mean":
         return np.mean(data)
 
     elif method == "nearest":
         array = np.asarray(sourceAxis)
         nearest_index_t = (np.abs(array - targetAxis[0])).argmin()
         return data[nearest_index_t]
-
-    else:
+    elif method == "linear" or method == "cubic":
         try:
             f = interp1d(sourceAxis, data, kind=method, bounds_error=True)
             return f(targetAxis)
@@ -70,6 +66,8 @@ def vertical_interpolation(sourceAxis,targetAxis,data,method):
             logging.warning("[InterpolatorCore][vertical_interpolation()] ----------------------------------------")
             f = interp1d(sourceAxis, data, kind=method,  fill_value = "extrapolate")
             return f(targetAxis)
+    else:
+        raise ValueError("Unable to decode vertical interpolation method : "+str(method))
 
 def time_1d_interpolation(sourceAxis,targetAxis,data,method):
     logging.debug("[InterpolatorCore][time_interpolation()] Looking for time : "+str(datetime.utcfromtimestamp(targetAxis[0]))+" with method '"+str(method)+"'.")