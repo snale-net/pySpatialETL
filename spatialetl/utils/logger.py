54a2eb078740015ff99826f94a2bfc76bd43fb67
Fabien Rétif <fabien.retif@zoho.com>
2020-02-19 15:04:26 +0100

    change logger behaviour

--- spatialetl/utils/logger.py
+++ spatialetl/utils/logger.py
@@ -3,15 +3,17 @@ import logging as log
 class myLogger(log.Logger):
 
     TIMING = 200
+    RUN = 25
     INFO = log.INFO
     DEBUG = log.DEBUG
-    WARN = log.WARN
+    WARNING = log.WARN
     ERROR = log.ERROR
     CRITICAL = log.CRITICAL
 
     def __init__(self,name):
         log.Logger.__init__(self,name)
         log.addLevelName(self.TIMING, 'TIMING')
+        log.addLevelName(self.RUN, 'RUN')
         log.basicConfig(format='[%(levelname)s] %(message)s')
 
     def debug(self, msg, *args, **kwargs):
@@ -19,13 +21,13 @@ class myLogger(log.Logger):
             self._log(self.DEBUG, msg, args, **kwargs)
 
     def info(self, msg, *args, **kwargs):
-
-        if self.isEnabledFor(self.INFO):
+        if self.isEnabledFor(self.INFO) or self.isEnabledFor(self.RUN):
             self._log(self.INFO, msg, args, **kwargs)
 
     def warning(self, msg, *args, **kwargs):
-        if self.isEnabledFor(self.WARN):
-            self._log(self.WARN, msg, args, **kwargs)
+        print(self.isEnabledFor(self.RUN))
+        if self.isEnabledFor(self.WARNING) and self.isEnabledFor(self.INFO) or not self.isEnabledFor(self.RUN):
+            self._log(self.WARNING, msg, args, **kwargs)
 
     def timing(self, msg, *args, **kwargs):
         if self.isEnabledFor(self.TIMING):
