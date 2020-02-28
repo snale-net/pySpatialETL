39c35a493a0bc688c5520d57a95d94d3bcf4eb6c
Fabien Rétif <fabien.retif@zoho.com>
2020-02-28 23:56:51 +0100

    update ww3 & ecmwf reader

--- spatialetl/coverage/io/netcdf/ecmwf/ECMWFReader.py
+++ spatialetl/coverage/io/netcdf/ecmwf/ECMWFReader.py
@@ -27,19 +27,36 @@ class ECMWFReader (CoverageReader):
 
     def __init__(self, myFile):
         CoverageReader.__init__(self,myFile);
+
         if os.path.isfile(self.filename):
             self.ncfile = Dataset(self.filename, 'r')
         elif os.path.isdir(self.filename):
-            self.ncfile = MFDataset(os.path.join(self.filename,"*.nc"), 'r')
+            self.ncfile = MFDataset(os.path.join(self.filename, "*.nc"), 'r')
+        elif self.filename.endswith("*"):
+            self.ncfile = MFDataset(self.filename + ".nc", 'r')
+        else:
+            raise ValueError("Unable to decode file " + str(self.filename))
 
-    def read_axis_x(self):
-        return self.ncfile.variables['lon'][:]
+    def is_regular_grid(self):
+        return True
 
-    def read_axis_y(self):
-        return self.ncfile.variables['lat'][:]
+    def get_x_size(self):
+        return np.shape(self.ncfile.variables['lon'][:])[0];
 
-    def read_axis_t(self, timestamp):
-        data = self.ncfile.variables['time'][:]
+    def get_y_size(self):
+        return np.shape(self.ncfile.variables['lat'][:])[0];
+
+    def get_t_size(self):
+        return np.shape(self.ncfile.variables['time'])[0];
+
+    def read_axis_x(self,xmin,xmax,ymin,ymax):
+        return self.ncfile.variables['lon'][xmin:xmax]
+
+    def read_axis_y(self,xmin,xmax,ymin,ymax):
+        return self.ncfile.variables['lat'][ymin:ymax]
+
+    def read_axis_t(self,tmin,tmax,timestamp):
+        data = self.ncfile.variables['time'][tmin:tmax]
         result = num2date(data, units=self.ncfile.variables['time'].units, calendar="gregorian")
 
         if timestamp == 1:
@@ -50,33 +67,33 @@ class ECMWFReader (CoverageReader):
 
     # Variables
 
-    def read_variable_longitude(self):
+    def read_variable_longitude(self,xmin,xmax,ymin,ymax):
         return self.read_axis_x()
 
-    def read_variable_latitude(self):
+    def read_variable_latitude(self,xmin,xmax,ymin,ymax):
         return self.read_axis_y()
 
-    def read_variable_time(self):
-        return self.read_axis_t(timestamp=0)
+    def read_variable_time(self,tmin,tmax):
+        return self.read_axis_t(tmin,tmax,timestamp=0)
 
-    def read_variable_2D_land_binary_mask(self):
-        mask = self.ncfile.variables["LSM"][0][:]
+    def read_variable_2D_land_binary_mask(self,xmin,xmax,ymin,ymax):
+        mask = np.ma.filled(self.ncfile.variables["LSM"][0,ymin:ymax,xmin:xmax], fill_value=np.nan)
         return mask
 
-    def read_variable_2D_sea_binary_mask(self):
-        mask = self.ncfile.variables["LSM"][0][:]
+    def read_variable_2D_sea_binary_mask(self,xmin,xmax,ymin,ymax):
+        mask = np.ma.filled(self.ncfile.variables["LSM"][0,ymin:ymax,xmin:xmax], fill_value=np.nan)
         mask += 1.0  # inverse le mask
         mask %= 2  # inverse le mask
         return mask
 
-    def read_variable_3D_sea_binary_mask_at_time(self, index_t):
-        mask = self.ncfile.variables["LSM"][index_t][:]
+    def read_variable_3D_sea_binary_mask_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        mask = np.ma.filled(self.ncfile.variables["LSM"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
         mask += 1.0  # inverse le mask
         mask %= 2  # inverse le mask
         return mask
 
-    def read_variable_3D_land_binary_mask_at_time(self, index_t):
-        mask = self.ncfile.variables["LSM"][index_t][:]
+    def read_variable_3D_land_binary_mask_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        mask = np.ma.filled(self.ncfile.variables["LSM"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
         return mask
 
     #################
@@ -84,52 +101,52 @@ class ECMWFReader (CoverageReader):
     # 2D
     #################
 
-    def read_variable_rainfall_amount_at_time(self, index_t):
-        return self.ncfile.variables["TP"][index_t][:]
+    def read_variable_rainfall_amount_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        return np.ma.filled(self.ncfile.variables["TP"][index_t][ymin:ymax,xmin:xmax], fill_value=np.nan)
 
     #################
     # METEO
     # Sea surface
     #################
 
-    def read_variable_surface_air_pressure_at_time(self, index_t):
-        sp = self.ncfile.variables["SP"][index_t][:]
+    def read_variable_surface_air_pressure_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        sp = np.ma.filled(self.ncfile.variables["SP"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
         sp *= 0.01  # Pa to hPa
         return sp
 
-    def read_variable_sea_surface_air_pressure_at_time(self,index_t):
-        sp = self.ncfile.variables["MSL"][index_t][:]
+    def read_variable_sea_surface_air_pressure_at_time(self,index_t,xmin,xmax,ymin,ymax):
+        sp = np.ma.filled(self.ncfile.variables["MSL"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
         sp *= 0.01  # Pa to hPa
         return sp
 
-    def read_variable_surface_downward_sensible_heat_flux_at_time(self, index_t):
-        return self.ncfile.variables["SSHF"][index_t][:]
+    def read_variable_surface_downward_sensible_heat_flux_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        return np.ma.filled(self.ncfile.variables["SSHF"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
 
-    def read_variable_surface_downward_latent_heat_flux_at_time(self, index_t):
-        return self.ncfile.variables["SLHF"][index_t][:]
+    def read_variable_surface_downward_latent_heat_flux_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        return np.ma.filled(self.ncfile.variables["SLHF"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
 
-    def read_variable_surface_air_temperature_at_time(self, index_t):
-        return self.ncfile.variables["T2M"][index_t][:]
+    def read_variable_surface_air_temperature_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        return np.ma.filled(self.ncfile.variables["T2M"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
 
-    def read_variable_dew_point_temperature_at_time(self, index_t):
-        return self.ncfile.variables["D2M"][index_t][:]
+    def read_variable_dew_point_temperature_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        return np.ma.filled(self.ncfile.variables["D2M"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
 
-    def read_variable_surface_downwards_solar_radiation_at_time(self, index_t):
-        return self.ncfile.variables["SSRD"][index_t][:]
+    def read_variable_surface_downwards_solar_radiation_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        return np.ma.filled(self.ncfile.variables["SSRD"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
 
-    def read_variable_surface_downwards_thermal_radiation_at_time(self, index_t):
-        return self.ncfile.variables["STRD"][index_t][:]
+    def read_variable_surface_downwards_thermal_radiation_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        return np.ma.filled(self.ncfile.variables["STRD"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
 
-    def read_variable_surface_solar_radiation_at_time(self, index_t):
-        return self.ncfile.variables["SSR"][index_t][:]
+    def read_variable_surface_solar_radiation_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        return np.ma.filled(self.ncfile.variables["SSR"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
 
-    def read_variable_surface_thermal_radiation_at_time(self, index_t):
-        return self.ncfile.variables["STR"][index_t][:]
+    def read_variable_surface_thermal_radiation_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        return np.ma.filled(self.ncfile.variables["STR"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)
 
     #################
     # METEO
     # At 10 m
     #################
 
-    def read_variable_wind_10m_at_time(self, index_t):
-        return [self.ncfile.variables["U10M"][index_t][:], self.ncfile.variables["V10M"][index_t][:]]
+    def read_variable_wind_10m_at_time(self, index_t,xmin,xmax,ymin,ymax):
+        return [np.ma.filled(self.ncfile.variables["U10M"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan), np.ma.filled(self.ncfile.variables["V10M"][index_t,ymin:ymax,xmin:xmax], fill_value=np.nan)]
--- spatialetl/coverage/io/netcdf/ww3/WW3Reader.py
+++ spatialetl/coverage/io/netcdf/ww3/WW3Reader.py
@@ -28,7 +28,7 @@ import os
 
 class WW3Reader (CoverageReader):
 
-    def __init__(self, myFile,isCurvilinear=False):
+    def __init__(self, myFile):
         CoverageReader.__init__(self,myFile);
 
         if os.path.isfile(self.filename):
@@ -36,7 +36,7 @@ class WW3Reader (CoverageReader):
         elif os.path.isdir(self.filename):
             self.ncfile = MFDataset(os.path.join(self.filename, "*.nc"), 'r')
 
-        if isCurvilinear:
+        if self.ncfile.variables['longitude'].ndim == 2:
             self.regular_grid=False
         else:
             self.regular_grid=True
@@ -87,72 +87,72 @@ class WW3Reader (CoverageReader):
     
     # Scalar 
     def read_variable_2D_sea_binary_mask(self,xmin,xmax,ymin,ymax):
-        mask = self.ncfile.variables["MAPSTA"][ymin:ymax,xmin:xmax]
+        mask = np.ma.filled(self.ncfile.variables["MAPSTA"][ymin:ymax,xmin:xmax], fill_value=np.nan)
         #mask += 1.0 # inverse le mask
         #mask %= 2 # inverse le mask
         return mask
 
     def read_variable_bathymetry(self,xmin,xmax,ymin,ymax):
-        return self.ncfile.variables["dpt"][0][ymin:ymax,xmin:xmax]
+        return np.ma.filled(self.ncfile.variables["dpt"][0][ymin:ymax,xmin:xmax], fill_value=np.nan)
 
     def read_variable_bathymetry_at_time(self,t,xmin,xmax,ymin,ymax):
-        return self.ncfile.variables["dpt"][t][ymin:ymax,xmin:xmax]
+        return np.ma.filled(self.ncfile.variables["dpt"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)
     
     def read_variable_sea_surface_height_above_mean_sea_level_at_time(self,t,xmin,xmax,ymin,ymax):
         return self.ncfile.variables["wlv"][t][ymin:ymax,xmin:xmax]
     
     def read_variable_sea_surface_wave_significant_height_at_time(self,t,xmin,xmax,ymin,ymax):
-        return self.ncfile.variables["hs"][t][ymin:ymax,xmin:xmax]
+        return np.ma.filled(self.ncfile.variables["hs"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)
 
     def read_variable_sea_surface_wave_breaking_height_at_time(self,t,xmin,xmax,ymin,ymax):
-        return self.ncfile.variables["wch"][t][ymin:ymax,xmin:xmax]
+        return np.ma.filled(self.ncfile.variables["wch"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)
     
     def read_variable_sea_surface_wave_from_direction_at_time(self,t,xmin,xmax,ymin,ymax):
         #attention en ce qui concerne les conventions d'angle en meteorologie. celles ci
         # sont appliquées par ww3. en meteo, un vent venant du nord a une direction de 0°,
         # un vent venant de l'est a une direction de 90°. par consequent il faut corriger
         # cette convention si on veut la direction vers en faisant 270°-angle
-        return self.ncfile.variables["dir"][t][ymin:ymax,xmin:xmax]
+        return np.ma.filled(self.ncfile.variables["dir"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)
 
     def read_variable_sea_surface_wave_to_direction_at_time(self, t,xmin,xmax,ymin,ymax):
         #attention en ce qui concerne les conventions d'angle en meteorologie. celles ci
         # sont appliquées par ww3. en meteo, un vent venant du nord a une direction de 0°,
         # un vent venant de l'est a une direction de 90°. par consequent il faut corriger
         # cette convention si on veut la direction vers en faisant 270°-angle
-        return 270.-self.ncfile.variables["dir"][t][ymin:ymax,xmin:xmax]
+        return 270.-np.ma.filled(self.ncfile.variables["dir"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)
     
     def read_variable_sea_surface_wave_mean_period_at_time(self,t,xmin,xmax,ymin,ymax):
-        return self.ncfile.variables["t01"][t][ymin:ymax,xmin:xmax]
+        return np.ma.filled(self.ncfile.variables["t01"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)
 
     def read_variable_sea_surface_wave_peak_period_at_time(self,t,xmin,xmax,ymin,ymax):
-        return self.ncfile.variables["tp"][t][ymin:ymax,xmin:xmax]
+        return np.ma.filled(self.ncfile.variables["tp"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)
 
     def read_variable_sea_surface_wave_energy_dissipation_at_ground_level_at_time(self,t,xmin,xmax,ymin,ymax):
-        return self.ncfile.variables["fbb"][t][ymin:ymax,xmin:xmax]
+        return np.ma.filled(self.ncfile.variables["fbb"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)
     
     def read_variable_radiation_pressure_bernouilli_head_at_time(self,t,xmin,xmax,ymin,ymax):
-        return self.ncfile.variables["bhd"][t][ymin:ymax,xmin:xmax]
+        return np.ma.filled(self.ncfile.variables["bhd"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)
 
     def read_variable_sea_surface_wave_energy_flux_to_ocean_at_time(self,t,xmin,xmax,ymin,ymax):
-        return self.ncfile.variables["foc"][t][ymin:ymax,xmin:xmax]
+        return np.ma.filled(self.ncfile.variables["foc"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)
 
     def read_variable_sea_surface_wave_peak_frequency_at_time(self,t,xmin,xmax,ymin,ymax):
-        return self.ncfile.variables["fp"][t][ymin:ymax,xmin:xmax]
+        return np.ma.filled(self.ncfile.variables["fp"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)
     
     # Vector
     def read_variable_barotropic_sea_water_velocity_at_time(self,t,xmin,xmax,ymin,ymax):
-        return [self.ncfile.variables["ucur"][t][ymin:ymax,xmin:xmax], self.ncfile.variables["vcur"][t][ymin:ymax,xmin:xmax]]
+        return [np.ma.filled(self.ncfile.variables["ucur"][t][ymin:ymax,xmin:xmax], fill_value=np.nan),np.ma.filled(self.ncfile.variables["vcur"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)]
 
     def read_variable_atmosphere_momentum_flux_to_waves_at_time(self,t,xmin,xmax,ymin,ymax):
-        return [self.ncfile.variables["utaw"][t][ymin:ymax,xmin:xmax],self.ncfile.variables["vtaw"][t][ymin:ymax,xmin:xmax]]
+        return [np.ma.filled(self.ncfile.variables["utaw"][t][ymin:ymax,xmin:xmax], fill_value=np.nan),np.ma.filled(self.ncfile.variables["vtaw"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)]
 
     def read_variable_waves_momentum_flux_to_ocean_at_time(self,t,xmin,xmax,ymin,ymax):
-        return [self.ncfile.variables["utwo"][t][ymin:ymax,xmin:xmax],self.ncfile.variables["vtwo"][t][ymin:ymax,xmin:xmax]]
+        return [np.ma.filled(self.ncfile.variables["utwo"][t][ymin:ymax,xmin:xmax], fill_value=np.nan),np.ma.filled(self.ncfile.variables["vtwo"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)]
 
     def read_variable_sea_surface_wave_stokes_drift_velocity_at_time(self,t,xmin,xmax,ymin,ymax):
-        return [self.ncfile.variables["uuss"][t][ymin:ymax,xmin:xmax],self.ncfile.variables["vuss"][t][ymin:ymax,xmin:xmax]]
+        return [np.ma.filled(self.ncfile.variables["uuss"][t][ymin:ymax,xmin:xmax], fill_value=np.nan),np.ma.filled(self.ncfile.variables["vuss"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)]
 
     def read_variable_wind_10m_at_time(self,t,xmin,xmax,ymin,ymax):
-        return [self.ncfile.variables["uwnd"][t][ymin:ymax,xmin:xmax],self.ncfile.variables["vwnd"][t][ymin:ymax,xmin:xmax]]
+        return [np.ma.filled(self.ncfile.variables["uwnd"][t][ymin:ymax,xmin:xmax], fill_value=np.nan),np.ma.filled(self.ncfile.variables["vwnd"][t][ymin:ymax,xmin:xmax], fill_value=np.nan)]
            
     
