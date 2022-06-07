08bd3bcd1e05e7b08766bee181397adc076913fb
Fabien Rétif <fabien.retif@zoho.com>
2022-06-07 20:19:02 +0200

    update snale writer

--- spatialetl/coverage/io/shape/SnaleWriter.py
+++ spatialetl/coverage/io/shape/SnaleWriter.py
@@ -16,12 +16,15 @@
 from __future__ import division, print_function, absolute_import
 
 import os
+from multiprocessing import Pool, cpu_count
 
+import geopandas as gpd
 import numpy as np
-from osgeo import gdal, gdal_array
-from osgeo import osr
+import pandas as pd
+from osgeo import gdal
 from osgeo import ogr
 from rasterio.crs import CRS
+from shapely.ops import unary_union
 
 from spatialetl.coverage.TimeCoverage import TimeCoverage
 from spatialetl.coverage.TimeLevelCoverage import TimeLevelCoverage
@@ -30,16 +33,11 @@ from spatialetl.exception.CoverageError import CoverageError
 from spatialetl.utils.VariableDefinition import VariableDefinition
 from spatialetl.utils.logger import logging
 
-import numpy as np
-import pandas as pd
-import geopandas as gpd
-from multiprocessing import Pool, cpu_count
-from shapely.ops import unary_union
 
 def union(x):
     return unary_union(x)
 
-class DefaultWriter (CoverageWriter):
+class SnaleWriter (CoverageWriter):
 
     def __init__(self,cov,myFile):
         CoverageWriter.__init__(self,cov,myFile);
@@ -122,18 +120,6 @@ class DefaultWriter (CoverageWriter):
                                                                                   'sea_water_column_thickness']) + '\' at time \'' + str(
                         time) + '\'')
 
-                    # file = self.driver.CreateDataSource(os.path.join(self.filename, time.strftime("%Y%m%d_%H%M%S") + "_" +
-                    #                                        VariableDefinition.VARIABLE_NAME[
-                    #                                            'sea_water_column_thickness'] + ".shp"))
-
-
-                    # CRS info
-                    # proj = osr.SpatialReference()
-                    # proj.SetWellKnownGeogCS("EPSG:4326")
-                    # layer = file.CreateLayer("s26", srs=proj)
-                    # fldSeverity = ogr.FieldDefn('Severity', ogr.OFTInteger)
-                    # layer.CreateField(fldSeverity)
-
                     global_data[time_index][global_data[time_index] == 9.96921e+36] = 0 # Remove NaN
                     global_data[time_index][global_data[time_index] <= 0.1] = 0
                     global_data[time_index][(global_data[time_index] > 1.2) & (global_data[time_index] != 0)] = 4
@@ -141,51 +127,41 @@ class DefaultWriter (CoverageWriter):
                     global_data[time_index][(global_data[time_index] > 0.3) & (global_data[time_index] <= 0.7)] = 2
                     global_data[time_index][(global_data[time_index] > 0.1) & (global_data[time_index] <= 0.3)] = 1
 
-                    # sq = gdal_array.OpenArray(global_data[time_index])
-                    # proj = osr.SpatialReference()
-                    # proj.SetWellKnownGeogCS("EPSG:4326")
-                    # sq.SetProjection(proj.ExportToWkt())
-                    # sq.SetGeoTransform(self.geotransform)
-                    # #gdal.Polygonize(sq.GetRasterBand(1), sq.GetRasterBand(1), layer, 0, ["8CONNECTED=2"], callback=None)
-                    # gdal.Polygonize(sq.GetRasterBand(1), sq.GetRasterBand(1), layer, 0, [], callback=None)
-
-
-                    print("Converting to GeoDataFrame...")
                     x, y, elevation = self.coverage.read_axis_x(type="target_global"), self.coverage.read_axis_y(type="target_global"),  global_data[time_index]
                     x, y = np.meshgrid(x, y)
                     x, y, elevation = x.flatten(), y.flatten(), elevation.flatten()
 
-                    dem_pd = pd.DataFrame.from_dict({'elevation': elevation, 'x': x, 'y': y})
-                    dem_threshold = 4
-                    dem_pd = dem_pd[dem_pd['elevation'] == dem_threshold]
-                    dem_vector = gpd.GeoDataFrame(
-                        geometry=gpd.GeoSeries.from_xy(dem_pd['x'], dem_pd['y'], crs= CRS.from_string('EPSG:4326')))
+                    for dem_threshold in range(1,4):
+                        dem_pd = pd.DataFrame.from_dict({'elevation': elevation, 'x': x, 'y': y})
+                        dem_pd = dem_pd[dem_pd['elevation'] == dem_threshold]
+                        dem_vector = gpd.GeoDataFrame(
+                            geometry=gpd.GeoSeries.from_xy(dem_pd['x'], dem_pd['y'], crs= CRS.from_string('EPSG:4326')))
 
-                    dem_vector = dem_vector.buffer(min(self.x_pixel_size,self.y_pixel_size)/2,cap_style=3)
-                    dem_vector = dem_vector.to_crs('EPSG:4326')
-                    geom_arr = []
-                    # Converting GeoSeries to list of geometries
-                    geoms = list(dem_vector)
+                        dem_vector = dem_vector.buffer(min(self.x_pixel_size,self.y_pixel_size)/2,cap_style=3)
+                        dem_vector = dem_vector.to_crs('EPSG:4326')
+                        geom_arr = []
+                        # Converting GeoSeries to list of geometries
+                        geoms = list(dem_vector)
 
-                    # Converting geometries list to nested list of geometries
-                    for i in range(0, len(geoms), 10000):
-                        geom_arr.append(geoms[i:i + 10000])
+                        # Converting geometries list to nested list of geometries
+                        for i in range(0, len(geoms), 10000):
+                            geom_arr.append(geoms[i:i + 10000])
 
-                    # Creating multiprocessing pool to perform union operation of chunks of geometries
-                    with Pool(cpu_count()) as p:
-                       geom_union = p.map(union, geom_arr)
+                        # Creating multiprocessing pool to perform union operation of chunks of geometries
+                        with Pool(cpu_count()) as p:
+                           geom_union = p.map(union, geom_arr)
 
-                    # Perform union operation on returned unioned geometries
-                    total_union = unary_union(geom_union)
+                        # Perform union operation on returned unioned geometries
+                        total_union = unary_union(geom_union)
 
-                    # Creating GeoDataFrame for total_union
-                    union_vector_gdf = gpd.GeoDataFrame(geometry=gpd.GeoSeries(total_union))
-                    union_vector_gdf['Severity'] = dem_threshold
+                        # Creating GeoDataFrame for total_union
+                        union_vector_gdf = gpd.GeoDataFrame(geometry=gpd.GeoSeries(total_union))
+                        union_vector_gdf['Severity'] = dem_threshold
 
-                    # Saving GeoDataFrame to shapefile
-                    union_vector_gdf.to_file(os.path.join(self.filename, time.strftime("%Y%m%d_%H%M%S") + "_" +
-                                                           VariableDefinition.VARIABLE_NAME[
-                                                               'sea_water_column_thickness'] +"_"+str(dem_threshold)+".shp"), crs='EPSG:4326')
+                        # Saving GeoDataFrame to shapefile
+                        union_vector_gdf.to_file(os.path.join(self.filename, time.strftime("%Y%m%d_%H%M%S") + "_" +
+                                                               VariableDefinition.VARIABLE_NAME[
+                                                                   'sea_water_column_thickness'] +"_severity-"+str(dem_threshold)+".shp"), crs='EPSG:4326')
         else:
             raise CoverageError("DefaultWriter",
                                 "The given coverage is not an instance of 'TimeCoverage' or 'TimeLevelCoverage'")
