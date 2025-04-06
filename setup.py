585aa0a75f605922f0f09dbc203a0a225ef677a1
Fabien Rétif <fabien.retif@snale.net>
2025-04-06 15:23:27 +0200

    Transform setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python2.7
-# -*- coding: utf-8 -*-
 # MIT License
 # Copyright (c) 2024 [SNALE - French SAS Company - RCS 951 724 616]
 #
@@ -20,50 +18,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import io
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-def read(*paths, **kwargs):
-    """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
-    '0.1.0'
-    >>> read("README.md")
-    ...
-    """
-
-    content = ""
-    with io.open(
-            os.path.join(os.path.dirname(__file__), *paths),
-            encoding=kwargs.get("encoding", "utf8"),
-    ) as open_file:
-        content = open_file.read().strip()
-    return content
-
-
-def read_requirements(path):
-    return [
-        line.strip()
-        for line in read(path).split("\n")
-        if not line.startswith(('"', "#", "-", "git+"))
-    ]
-
-setup (
-       name='spatialetl',
-       version=read("VERSION"),
-       packages=find_packages(exclude=("spatialetl.providers","spatialetl.providers.*","spatialetl.extensions","spatialetl.extensions.*")),
-
-       # Declare your packages' dependencies here, for eg:
-       python_requires='>=3.9',
-       install_requires=read_requirements("requirements.txt"),
-
-       author='Fabien RÉTIF',
-       author_email='fabien.retif@snale.net',
-
-       #summary = 'Just another Python package for the cheese shop',
-       url='https://lab.snale.net/produits/pyspatialetl',
-       license='',
-       long_description=' Extract - Transform - Load (ETL) of geospatial data, that is, digital geospatial information representing space and time-varying phenomena. This python package deals with multi-dimensional gridded data and multi-dimensional multi-point data.',
-
-       # could also include long_description, download_url, classifiers, etc.
-       )
+setup()
