df4960524f7078b36dbe0349ae656f45f9f2c5c5
Benjamin Guenoun <benjamin.guenoun@snale.net>
2025-05-23 08:30:07 +0200

    [refactor]: rename providers_meteofrance module class file names snake_case

--- spatialetl/providers/meteofrance/spatialetl/coverage/io/tiff/mf/__init__.py
+++ spatialetl/providers/meteofrance/spatialetl/coverage/io/tiff/mf/__init__.py
@@ -9,6 +9,6 @@ TIFF METEO-FRANCE (:mod:`coverage.io.tiff.mf`)
 """
 from __future__ import division, print_function, absolute_import
 
-from .INSPIREReader import INSPIREReader
+from .inspire_reader import INSPIREReader
 
 __all__ = ['INSPIREReader']
