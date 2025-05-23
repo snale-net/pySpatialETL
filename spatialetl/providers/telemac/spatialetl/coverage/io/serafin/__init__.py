20be68936010198d240629b70c1e82e7214b9b7a
Benjamin Guenoun <benjamin.guenoun@snale.net>
2025-05-23 08:43:11 +0200

    [refactor]: rename providers_telemac module class file names snake_case

--- spatialetl/providers/telemac/spatialetl/coverage/io/serafin/__init__.py
+++ spatialetl/providers/telemac/spatialetl/coverage/io/serafin/__init__.py
@@ -9,8 +9,8 @@ SERAFIN (:mod:`coverage.io.serafin`)
 """
 from __future__ import division, print_function, absolute_import
 
-from .SerafinHeader import SerafinHeader
-from .SerafinReader import SerafinReader
-from .SerafinWriter import SerafinWriter
+from .serafin_header import SerafinHeader
+from .serafin_reader import SerafinReader
+from .serafin_writer import SerafinWriter
 
 __all__ = ['SerafinHeader', 'SerafinReader', 'SerafinWriter']
